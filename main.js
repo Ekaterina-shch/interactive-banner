d2c5cc189f3e377c0b75332b540f99e966614fe2
Ekaterina <shch.e.v@yandex.ru>
2024-10-20 21:20:22 +0700

    add button

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const containers = document.querySelectorAll('.container');
 const basket = document.getElementById('basket');
 const wrapper = document.querySelector('.shop-wrapper');
 
-products.forEach(handleDrag);
+let isInBasket = false;
+
+products.forEach(dragAndDrop);
 
 containers.forEach((container) => {
   container.addEventListener('dragover', (e) => {
@@ -13,7 +15,7 @@ containers.forEach((container) => {
   });
 });
 
-function handleDrag(product) {
+function dragAndDrop(product) {
   product.addEventListener('dragstart', () =>
     product.classList.add('dragging')
   );
@@ -32,7 +34,7 @@ function handleDrag(product) {
   product.addEventListener('touchmove', (e) => {
     if (isDragging) {
       product.classList.add('dragging');
-      moveAt(e.touches[0]);
+      moveAt(e.touches[0], product);
       checkDropZone(e.touches[0]);
     }
   });
@@ -41,37 +43,40 @@ function handleDrag(product) {
     if (isDragging) {
       isDragging = false;
       product.classList.remove('dragging');
-      if (basket.classList.contains('active')) {
-        addToBasket();
+      if (isInBasket) {
+        addToBasket(product);
         product.style.top = '';
         product.style.left = '';
-        basket.classList.remove('active');
+        isInBasket = false;
       }
     }
   });
+}
 
-  function moveAt(touch) {
-    product.style.top = `${
-      touch.pageY - wrapper.offsetTop - product.offsetHeight / 2
-    }px`;
-    product.style.left = `${
-      touch.pageX - wrapper.offsetLeft - product.offsetWidth / 2
-    }px`;
-  }
+function moveAt(touch, product) {
+  product.style.top = `${
+    touch.clientY - wrapper.offsetTop - product.offsetHeight / 2
+  }px`;
+  product.style.left = `${
+    touch.clientX - wrapper.offsetLeft - product.offsetWidth / 2
+  }px`;
+}
 
-  function checkDropZone(touch) {
-    const rect = basket.getBoundingClientRect();
-    if (
-      touch.clientX >= rect.left &&
-      touch.clientX <= rect.right &&
-      touch.clientY >= rect.top &&
-      touch.clientY <= rect.bottom
-    ) {
-      basket.classList.add('active');
-    }
+function checkDropZone(touch) {
+  const rect = basket.getBoundingClientRect();
+  if (
+    touch.clientX >= rect.left &&
+    touch.clientX <= rect.right &&
+    touch.clientY >= rect.top &&
+    touch.clientY <= rect.bottom
+  ) {
+    isInBasket = true;
+    console.log('мы над корзиной');
   }
+}
 
-  function addToBasket() {
-    basket.appendChild(product);
-  }
+function addToBasket(product) {
+  containers.forEach((container) => {
+    container.appendChild(product);
+  });
 }
